f9a8163684985edcff0f1a875e0098b079e92dd9
Michael Yankura <myankura210@gmail.com>
2019-03-01 15:12:01 -0600

    Push up daily-journal2 to github

---
+++ scripts/journal.js
@@ -0,0 +1,33 @@
+/*
+    Define the keys and value for a JavaScript object that
+    represents a journal entry about what you learned today
+*/
+const objectsJournalEntry = {
+    journalDate : "",
+    conceptsCovered : "",
+    journalEntry : "",
+    mood :""
+}
+const objectsJournalEntry1 = {
+    journalDate : "02/25/19",
+    conceptsCovered : "GitHub",
+    journalEntry : "Today we learned how to use GitHub for the first time.",
+    mood :"ok"
+}
+const objectsJournalEntry2 = {
+    journalDate : "02/27/19",
+    conceptsCovered : "Working with the DOM",
+    journalEntry : "Today we started working with the DOM",
+    mood :"ok"
+}
+const objectsJournalEntry3 = {
+    journalDate : "02/28/19",
+    conceptsCovered : "Beggining of JavaScript",
+    journalEntry : "We have officially started working with JavaScript learning about functions, objects, and loops.  Shit is moving fast yo.",
+    mood :"ok"
+}
+let arrayJournalEntry = [];
+
+arrayJournalEntry.push(objectsJournalEntry1);
+arrayJournalEntry.push(objectsJournalEntry2);
+arrayJournalEntry.push(objectsJournalEntry3);
