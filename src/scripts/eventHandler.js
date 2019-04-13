08376ff2e0509fc47dba0d26bec3ad5c9ac1163b
Michael Yankura <myankura210@gmail.com>
2019-04-13 12:30:52 -0500

    added eventlistener and http post. getting 500 internal server error

--- src/scripts/eventHandler.js
+++ src/scripts/eventHandler.js
@@ -1,4 +1,21 @@
-const testButton = () => {
-    console.log("The button has been clicked.")
+// const handleSave = document.querySelector("#submit--save").addEventListener("click", event
 
-}
+const handleSave = () => {
+    const conceptInput = document.querySelector("#concept--input").value;
+    const entryInput = document.querySelector("#journalEntry--input").value;
+    const dateInput = document.querySelector("#date--input").value;
+    const moodInput = document.querySelector("#mood--select").value;
+
+
+    console.log("The save button has been clicked.")
+    const newEntry = {
+        concept: conceptInput,
+        entry: entryInput,
+        date: dateInput,
+        mood: moodInput
+    }
+    console.log("new entry: ", newEntry);
+    //post the entry to journal
+    postEntry();
+
+};
--- src/scripts/journal.js
+++ src/scripts/journal.js
@@ -1,32 +1,35 @@
 //Create a form for inputing daily journal entries
 displayContainer.appendChild(buildElement("section", "entry--form"));
+
 const formContainer = document.querySelector("#entry--form");
+
 //concept covered
 formContainer.appendChild(buildElement("label", "concept--label", "Concept covered: "));
 formContainer.appendChild(buildElement("input", "concept--input"));
+
 //journal entry
 formContainer.appendChild(buildElement("label", "journalEntry--label", "Journal Entry: "));
 formContainer.appendChild(buildElement("textArea", "journalEntry--input"));
-//let textAreaInput = formContainer.appendChild(buildElement("textArea", "journalEntry--input"));
-//sizes the textArea element. It was a nice idea but it didn't pan out because of flexbox.
-// textAreaInput.rows = "10";
-// textAreaInput.cols = "5";
-//date of journal entry
+
+//date of entry
 formContainer.appendChild(buildElement("label", "date--label", "Date covered: "));
 let dateInput = formContainer.appendChild(buildElement("input", "date--input"));
 dateInput.setAttribute("type", "date");
+
 //mood of the day -- this is where the select feature and options are built
 formContainer.appendChild(buildElement("label", "mood--label", "Mood: "));
 let selectElement = formContainer.appendChild(buildElement("select", "mood--select"));
 selectElement.appendChild(buildOption("happy", "Happy!"));
 selectElement.appendChild(buildOption("ok", "Ok"));
 selectElement.appendChild(buildOption("sad", "Sad"));
+
+
 //submit button
 formContainer.appendChild(buildElement("button", "submit--button", "Submit Entry"));
-
 //create a variable that targets the submit button
 let submitButton = document.querySelector("#submit--button")
-submitButton.addEventListener("click", testButton)
+submitButton.addEventListener("click", handleSave)
+
 
 //Create a section for the entries to be displayed beneath the form
 displayContainer.appendChild(buildElement("section", "entry--log"));
